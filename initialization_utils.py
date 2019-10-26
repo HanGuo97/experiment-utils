37e4752ed1c967cab5d30e827dcd7b0f91928f2f
HanGuo97 <alex.guohan1106@gmail.com>
2019-10-26 17:15:50 -0400

    initialization_utils: pascal-case

--- initialization_utils.py
+++ initialization_utils.py
@@ -1,6 +1,5 @@
 import os
 import click
-import stringcase
 from git import Repo
 from absl import logging
 from datetime import datetime
@@ -47,8 +46,10 @@ def interactive_initialize(
         "Please Enter The Experiment Description",
         type=str, default=default_experiment_description)
 
-    # Tag should be in camel-case
-    experiment_tag = stringcase.camelcase(experiment_tag)
+    # Project Name should be in camel-case
+    project_name = to_PascalCase(project_name)
+    # Experimetn Tag should be in camel-case
+    experiment_tag = to_PascalCase(experiment_tag)
     # Experiment Name will include more info
     experiment_name, git_repo = _get_experiment_name(tag=experiment_tag)
     # Experiment log dir
@@ -92,3 +93,7 @@ def _print_config() -> None:
         click.echo(f"{LOG_STRING} "
                    f"{CONFIG._fields[i]: <{max_length}}: "
                    f"{CONFIG[i]}")
+
+
+def to_PascalCase(s: str) -> str:
+    return "".join(x for x in s.title() if not x.isspace())
